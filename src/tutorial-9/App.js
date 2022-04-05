8bb43abba1cbe314fc49df00a1bb01c8eecc8043
Liam <backgroundpositioncenter@gmail.com>
2022-04-05 10:45:44 +0300

    Поправил обработку ошибок

--- src/tutorial-9/App.js
+++ src/tutorial-9/App.js
@@ -3,16 +3,18 @@ import React from "react";
 const App = () => {
   const [users, setUsers] = React.useState([]);
 
-  async function getUsers() {
-    try {
-      const data = await fetch(
-        "https://62487b0a20197bb46269d271.mockapi.io/userss"
-      );
-      const result = await data.json();
-      setUsers(result);
-    } catch (e) {
-      console.error(e);
+  async function getUsers(e) {
+    // e.preventDefault();
+
+    const response = await fetch(
+      "https://62487b0a20197bb46269d271.mockapi.io/user"
+    );
+    if (!response.ok) {
+      const messageError = `Произошла ошибка: ${response.status}`;
+      throw Error(messageError);
     }
+    const result = await response.json();
+    setUsers(result);
   }
 
   return (
