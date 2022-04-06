249798e4a1bddc98f18c367ec435e89588bccaa4
Liam <backgroundpositioncenter@gmail.com>
2022-04-07 00:27:30 +0300

    Сверстал интерфейс приложения для просмотра GH-аккаунта

--- src/tutorial-9/App.js
+++ src/tutorial-9/App.js
@@ -1,31 +1,56 @@
 import React from "react";
+import avatar from "./assets/Owl.png";
 
-const App = () => {
-  const [users, setUsers] = React.useState([]);
-
-  async function getUsers(e) {
-    // e.preventDefault();
-
-    const response = await fetch(
-      "https://62487b0a20197bb46269d271.mockapi.io/user"
-    );
-    if (!response.ok) {
-      const messageError = `Произошла ошибка: ${response.status}`;
-      throw Error(messageError);
-    }
-    const result = await response.json();
-    setUsers(result);
-  }
-
+function App() {
   return (
-    <div>
-      <button onClick={getUsers}>Получить список пользователей</button>
-      <ul>
-        {users.length &&
-          users.map((item) => <li key={item.id}>{item.name}</li>)}
-      </ul>
+    <div className="app">
+      <div className="app-container">
+        <form className="app-form">
+          <input
+            className="app-form_input"
+            placeholder="Укажите GitHub-аккаунт"
+          />
+          <button className="app-form_button">Найти</button>
+        </form>
+        <div className="app-user">
+          <div className="app-user_info">
+            <div className="app-user_image">
+              <img src={avatar} alt="" />
+            </div>
+            <div className="app-user_data">
+              <h1 className="app-user_name">
+                Liam Sav <br />
+                <span>@Isaev3-IV</span>
+              </h1>
+              <p className="app-user_about">
+                Frontend Developer. UI Designer. JavaScript ❤️ ReactJS ⚛ React
+                Native, NodeJS, PHP
+              </p>
+            </div>
+          </div>
+          <ul className="app-user_stats">
+            <li className="app-user_stats-item">
+              Репозитории <br />
+              <span>124</span>
+            </li>
+            <li className="app-user_stats-item">
+              Подписчиков <br />
+              <span>1.2к</span>
+            </li>
+            <li className="app-user_stats-item">
+              Звезд <br />
+              <span>506</span>
+            </li>
+          </ul>
+          <ul className="app-user_location">
+            <li className="app-user_location-item">Russia, Moscow</li>
+            <li className="app-user_location-item">
+              https://github.com/Isaev3-IV
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-};
-
+}
 export default App;
